fcc91601a9fce0dd957b6631477711c62c918838
AkhileshS123 <akhilesh.sivaganesan@gmail.com>
2023-01-11 12:32:49 -0500

    Added docker configuration

--- DjangoRestApisPostgreSQL/settings.py
+++ DjangoRestApisPostgreSQL/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
